33c11d80212da4fe157fc0540dc7a68e669c64da
knightsljx <110027963+knightsljx@users.noreply.github.com>
2024-02-07 11:29:48 +0800

    Update test.ts

--- test.ts
+++ test.ts
@@ -1,1 +1,12 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+import Math
+
+def on_forever():
+    adcValue = pins.analog_read_pin(AnalogPin.P0)
+    V = adcValue*3.3/1023.0
+    Rt = V/((3.3-V)/10)
+    tempC = (1/(1/(273.15+25) + Math.log(Rt/10)/3950))-273.15
+    basic.show_number(tempC)
+    pass
+basic.forever(on_forever)
+
+serial.write_number(tempC)
