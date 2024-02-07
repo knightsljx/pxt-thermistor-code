abc385538983f27496e451e3d994ac806bec9d93
knightsljx <110027963+knightsljx@users.noreply.github.com>
2024-02-07 11:42:13 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,9 +1,11 @@
-basic.forever(function on_forever() {
-    let adcValue = pins.analogReadPin(AnalogPin.P0)
-    let V = adcValue * 3.3 / 1023.0
-    let Rt = V / ((3.3 - V) / 10)
-    let tempC = 1 / (1 / (273.15 + 25) + Math.log(Rt / 10) / 3950) - 273.15
-    basic.showNumber(tempC)
-    
+//% color="#AA278D"
+namespace hello {
+    //% block="say hello"
+    export function sayHello() {
+        console.log("Hello World");
+    }
+}
+
+input.onButtonPressed(Button.A, function () {
+    hello.sayHello();
 })
-serial.writeNumber(0)
