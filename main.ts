cc48fef707efd733cc30cef12bb3994c3f1cf129
knightsljx <110027963+knightsljx@users.noreply.github.com>
2024-02-07 14:29:38 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,11 +1,12 @@
-//% color="#AA278D"
-namespace hello {
-    //% block="say hello"
-    export function sayHello() {
-        console.log("Hello World");
+//% color=#f44242 icon="\uf1b2" block="2D6"
+namespace dices {
+    //% blockId=dices_2d6 block="2D6"
+    //% weight=100 blockGap=8
+    export function roll2D6(): number {
+        // generate two random numbers from 1 to 6
+        let dice1 = Math.randomRange(1, 6);
+        let dice2 = Math.randomRange(1, 6);
+        // return the sum of the two dice
+        return dice1 + dice2;
     }
 }
-
-input.onButtonPressed(Button.A, function () {
-    hello.sayHello();
-})
